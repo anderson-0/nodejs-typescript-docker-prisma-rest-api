a17cdca9cb5d3e0b67832b0504edd8ee48010f85
Anderson <anderson.consultant.it@outlook.com>
2021-12-12 18:43:58 -0500

    feature: completed sentry config + env.example database entries

--- src/app.ts
+++ src/app.ts
@@ -3,6 +3,7 @@ import "dotenv/config";
 import cors from "cors";
 import express, { NextFunction, Request, Response } from "express";
 import * as Sentry from "@sentry/node";
+import * as Tracing from "@sentry/tracing";
 import "express-async-errors";
 
 import "@shared/infra/orm/prisma";
@@ -14,12 +15,20 @@ const app = express();
 
 Sentry.init({
   dsn: process.env.SENTRY_DSN,
+  integrations: [
+    // enable HTTP calls tracing
+    new Sentry.Integrations.Http({ tracing: true }),
+    // enable Express.js middleware tracing
+    new Tracing.Integrations.Express({ app }),
+  ],
   tracesSampleRate: 1.0,
-  environment: process.env.NODE_ENV,
 });
 
-// The request handler must be the first middleware on the app
+// RequestHandler creates a separate execution context using domains, so that every
+// transaction/span/breadcrumb is attached to its own Hub instance
 app.use(Sentry.Handlers.requestHandler());
+// TracingHandler creates a trace for every incoming request
+app.use(Sentry.Handlers.tracingHandler());
 
 app.use(cors());
 app.use(express.json());
